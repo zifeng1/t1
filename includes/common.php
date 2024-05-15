1a159116e432e13caf41ade73def5941d40335e3
zifeng1 <phangzf-am22@student.tarc.edu.my>
2024-05-15 21:56:21 +0800

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("t1.chaw264c6t0p.us-east-1.rds.amazonaws.com","admin","12345678","ecommerce");
+$con=mysqli_connect("t1.cbikw04eie8m.us-east-1.rds.amazonaws.com","admin","12345678","ecommerce");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
